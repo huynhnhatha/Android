73d96d783ef7ff49875ff2df59c48e2d9a089af9
huynhnhatha <pepoohcool119@gmail.com>
2017-05-17 18:45:00 +0700

    code

---
+++ Vidu_ListView_ArrayList_Object/src/com/example/vidu_listview_arraylist_object/EmployeePartTime.java
@@ -0,0 +1,15 @@
+package com.example.vidu_listview_arraylist_object;
+
+public class EmployeePartTime extends Employee {
+	 
+public double TinhLuong() {
+ // TODO Auto-generated method stub
+ return 150;
+ }
+ @Override
+ public String toString() {
+ // TODO Auto-generated method stub
+ return super.toString() +" -->PartTime="+TinhLuong();
+ }
+}
+
